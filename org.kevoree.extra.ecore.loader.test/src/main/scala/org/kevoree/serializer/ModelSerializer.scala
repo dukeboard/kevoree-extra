21380b1d164ff07bd40c710b87be8888beee1c21
duke <fouquet.f@gmail.com>
2011-10-03 18:40:45 +0200

    clean

--- org.kevoree.extra.ecore.loader.test/src/main/scala/org/kevoree/serializer/ModelSerializer.scala
+++
@@ -1,9 +0,0 @@
-package org.kevoree.serializer
-class ModelSerializer extends ContainerRootSerializer {
-def serialize(o : Object) : String = {
-o match {
-case o : org.kevoree.ContainerRoot => ContainerRoottoXmi(o).toString()
-case _ => null
-}
-}
-}
