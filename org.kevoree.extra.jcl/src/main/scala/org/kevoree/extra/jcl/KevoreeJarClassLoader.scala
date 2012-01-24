d8816d3b7dd115a0930d17761fb03305fa24112e
duke <duke@duke.irisa.fr>
2012-01-24 19:38:05 +0100

    Add JCL

---
+++ org.kevoree.extra.jcl/src/main/scala/org/kevoree/extra/jcl/KevoreeJarClassLoader.scala
@@ -0,0 +1,40 @@
+package org.kevoree.extra.jcl
+
+import org.xeustechnologies.jcl.JarClassLoader
+import java.io.{ByteArrayInputStream, InputStream}
+import java.lang.String
+import java.net.URL
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 23/01/12
+ * Time: 18:57
+ * To change this template use File | Settings | File Templates.
+ */
+
+class KevoreeJarClassLoader extends JarClassLoader {
+
+  classpathResources =  new KevoreeLazyJarResources
+
+  override def getResourceAsStream(name  : String) : InputStream = {
+    val res = this.classpathResources.getResource(name)
+    if(res != null){
+      new ByteArrayInputStream(res)
+    } else {
+      null
+    }
+  }
+
+  override def getResource(p1: String): URL = {
+    classpathResources.asInstanceOf[KevoreeLazyJarResources].getContentURL(p1)
+  }
+
+  def unload() {
+    import scala.collection.JavaConversions._
+    (this.getLoadedClasses.keySet().toList++List()).foreach{ lc =>
+      unloadClass(lc)
+    }
+  }
+
+}
