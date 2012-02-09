ca1bed00670b6181a5a2b9066961f72171e790c5
François Fouquet <fouquet.f@gmail.com>
2012-02-09 18:33:08 +0100

    test

--- org.kevoree.extra.kserial/src/main/java/org/kevoree/extra/kserial/Test.java
+++ org.kevoree.extra.kserial/src/main/java/org/kevoree/extra/kserial/Test.java
@@ -3,7 +3,6 @@ package org.kevoree.extra.kserial;
 import org.kevoree.extra.kserial.Flash.FlashFirmware;
 import org.kevoree.extra.kserial.Flash.FlashFirmwareEvent;
 import org.kevoree.extra.kserial.Flash.FlashFirmwareEventListener;
-import org.kevoree.extra.kserial.SerialPort.*;
 import org.kevoree.extra.kserial.Utils.KHelpers;
 
 public class Test {
@@ -43,26 +42,20 @@ public class Test {
                  */
 
 
+        FlashFirmware flash = new FlashFirmware("/dev/tty.usbserial-A5005A2D", "ATMEGA328", "NODE0");
 
-  FlashFirmware flash = new FlashFirmware("/dev/tty.usbserial-A400fXsq","ATMEGA328","NODE0");
+        Byte[] intel = KHelpers.read_file("/Users/duke/Documents/dev/dukeboard/kevoree-extra/org.kevoree.extra.kserial/src/main/c/FlashOvertheair/program_test/test.hex");
+        if (flash.write_on_the_air_program(intel) >= 0) {
+            flash.addEventListener(new FlashFirmwareEventListener() {
+                // @Override
+                public void FlashEvent(FlashFirmwareEvent evt) {
+                    System.out.println("sent " + evt.getSize_uploaded());
+                }
+            });
 
-  Byte[] intel = KHelpers.read_file("/Users/oxyss35/kevoree-extra/org.kevoree.extra.kserial/src/main/c/FlashOvertheair/program_test/test.hex");
-  if(flash.write_on_the_air_program(intel) >= 0){
-      flash.addEventListener(new FlashFirmwareEventListener() {
-                  // @Override
-                  public void FlashEvent(FlashFirmwareEvent evt) {
-                      System.out.println("sent "+evt.getSize_uploaded());
-                  }
-              });
+            Thread.currentThread().sleep(1000000);
 
-      Thread.currentThread().sleep(1000000);
-
-  }
-
-
-
-
-
+        }
 
 
     }
