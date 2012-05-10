347dc2415685adf947cfa80ff3253b45441d82aa
Erwan Daubert <erwan.daubert@gmail.com>
2012-05-10 12:59:06 +0200

    update jackson library dependency

--- org.kevoree.extra.marshalling/src/main/scala/org/kevoree/extra/marshalling/JacksonSerializer.scala
+++ org.kevoree.extra.marshalling/src/main/scala/org/kevoree/extra/marshalling/JacksonSerializer.scala
@@ -3,7 +3,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,40 +12,45 @@
  * limitations under the License.
  */
 /**
-* Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* 	http://www.gnu.org/licenses/lgpl-3.0.txt
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.kevoree.extra.marshalling
 
 
-import org.codehaus.jackson.map.ObjectMapper
 import scala.runtime.BoxedUnit
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.databind.ObjectMapper
 
 
-case class RichJSONObject(obj : Any) {
-  def toJSON : String = {
-    if(obj.isInstanceOf[Unit] || obj.isInstanceOf[BoxedUnit] ){
+case class RichJSONObject (obj: Any) {
+  def toJSON: String = {
+    println("titi")
+    if (obj.isInstanceOf[Unit] || obj.isInstanceOf[BoxedUnit]) {
       return "<void>"
     }
-    JacksonSerializer.mapper.writeValueAsString(obj)
+    println("1" + obj) // TODO the following line is locked
+    val result = JacksonSerializer.mapper.writeValueAsString(obj)
+    println("2")
+    result
   }
 }
 
-case class RichString(s : String) {
-  def fromJSON[A](c : Class[A]) : A = {
+case class RichString (s: String) {
+  def fromJSON[A] (c: Class[A]): A = {
 
     s match {
-      case "<void>" => return null.asInstanceOf[A]
+      case "<void>" => null.asInstanceOf[A]
       case _ => JacksonSerializer.mapper.readValue(s, c).asInstanceOf[A]
     }
 
@@ -56,8 +61,12 @@ case class RichString(s : String) {
 
 object JacksonSerializer {
   var mapper = new ObjectMapper();
+
+  mapper.registerModule(DefaultScalaModule)
+
   //mapper.configure(SerializationConfig.Feature.WRAP_ROOT_VALUE, true)
-  implicit def convToJSON(obj : Any) = RichJSONObject(obj)
-  implicit def convFromJSON(c : String) = RichString(c)
+  implicit def convToJSON (obj: Any) = RichJSONObject(obj)
+
+  implicit def convFromJSON (c: String) = RichString(c)
 }
 
